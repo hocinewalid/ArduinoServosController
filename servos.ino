7aa2c2304599c62b9089b5be1e2b491ed8a9adca
Walid Hocine <30287336+hocinewalid@users.noreply.github.com>
2018-04-08 12:52:43 +0200

    Code Arduino pour controller un cerveau-moteur

---
+++ servos.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int val;
+
+void setup (){
+  Serial.begin(9600);
+  myservo.attach(9);
+  
+  
+  
+  }
+
+  void loop(){
+
+    
+  }
+
+
+void serialEvent(){
+
+  val  = Serial.parseInt();
+  if(val= 0){
+
+    myservo.write(val);
+  }
+  
+}
+
+
+
