8248bbbf56dfaaf003c759b723fb5e6218275447
walid-dev <walid-dev@outlook.com>
2018-04-08 11:49:29 +0100

    Add project files.

---
+++ ArduinoServosController/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ArduinoServosController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.degreelab = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Black;
+            this.trackBar1.Location = new System.Drawing.Point(72, 39);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(474, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0°";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(553, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "180°";
+            // 
+            // degreelab
+            // 
+            this.degreelab.AutoSize = true;
+            this.degreelab.BackColor = System.Drawing.Color.Navy;
+            this.degreelab.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.degreelab.Location = new System.Drawing.Point(273, 120);
+            this.degreelab.Name = "degreelab";
+            this.degreelab.Size = new System.Drawing.Size(70, 16);
+            this.degreelab.TabIndex = 3;
+            this.degreelab.Text = "Degree =  ";
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::ArduinoServosController.Properties.Resources.wallhaven_294154;
+            this.ClientSize = new System.Drawing.Size(606, 201);
+            this.Controls.Add(this.degreelab);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(622, 240);
+            this.MinimumSize = new System.Drawing.Size(622, 240);
+            this.Name = "Form1";
+            this.Text = "Walid";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label degreelab;
+    }
+}
+
---
+++ ArduinoServosController/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoServosController
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+            initfunction();
+        }
+
+        public void initfunction()
+        {
+
+            port = new SerialPort();
+            port.PortName = "COM6";
+            port.BaudRate = 9600;
+
+
+            try
+            {
+
+                port.Open();
+            }
+            catch (Exception e1) { 
+
+            MessageBox.Show(e1.Message);
+         }
+
+
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            if (port.IsOpen)
+            {
+
+                port.WriteLine(trackBar1.Value.ToString());
+                degreelab.Text = "Degree = " + trackBar1.Value.ToString(); 
+
+
+
+            }
+
+        }
+
+        
+    }
+}
